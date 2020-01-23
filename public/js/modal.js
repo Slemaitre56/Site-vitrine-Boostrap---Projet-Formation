89ad24bb9a478b385f79553aba021007a422f136
Stephanie LEMAITRE <stephanie.lemaitre56@gmail.com>
2020-01-23 13:59:37 +0100

    correction similar code #14

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,9 @@
 let modal = document.getElementById("envoyer");
 
-let mod = document.getElementById("mod");
-let mod1 = document.getElementById("mod1");
-let mod2 = document.getElementById("mod2");
-let mod3 = document.getElementById("mod3");
+let modName = document.getElementById("mod");
+let modSurname = document.getElementById("mod1");
+let modMail = document.getElementById("mod2");
+let modPassword = document.getElementById("mod3");
 
 let namemod = document.getElementById("nomM");
 let nameValid = /^([a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]{2,})+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]{2,})?$/;
@@ -17,77 +17,28 @@ let regexMailM = /[a-zA-Z0-9._-]{3,}@[a-zA-Z0-9._-]{2,}(\.fr|\.com)$/;
 let passwordM = document.getElementById("pwdM");
 let regexPwdM = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\.])(?=.{8,})/;
 
-
-// nom et prenom
-modal.addEventListener('click', nomModal)
+modal.addEventListener('click', valModalAll)
 
 
-function nomModal(event) {
-
-    if (namemod.validity.valueMissing) {
-        event.preventDefault();
-        mod.innerHTML="&#xf12a";
-
-    } else if (nameValid.test(namemod.value) == false) {
-        event.preventDefault();
-        mod.innerHTML="&#xf00d";
-
-    } else {
-        mod.innerHTML="&#xf00c";
-        event.preventDefault();
-    }
-};
-
-modal.addEventListener('click', prenomModal);
-function prenomModal(event) {
-
-    if (prenommod.validity.valueMissing) {
-        event.preventDefault();
-        mod1.innerHTML="&#xf12a";
-
-    } else if (prenommodValid.test(prenommod.value) == false) {
-        event.preventDefault();
-        mod1.innerHTML="&#xf00d";
-
-    } else {
-        event.preventDefault();
-        mod1.innerHTML="&#xf00c";
-    }
-};
-modal.addEventListener('click',modalMail );
-function modalMail(event) {
+function valModalAll(event) {
+    valid(namemod, nameValid, modName, event);  
+    valid(prenommod, prenommodValid, modSurname, event);
+    valid(mailM , regexMailM, modMail, event);
+    valid(passwordM, regexPwdM, modPassword, event);
+    
+ };
 
 
-    if (mailM.validity.valueMissing) {
-        event.preventDefault();
-        mod2.innerHTML="&#xf12a";
+function valid(val, validVal, wrapper, event) {
+    if (val.validity.valueMissing) { 
+        event.preventDefault(); 
+        wrapper.innerHTML="&#xf12a";
+ 
+    } else if (validVal.test(val.value) == false) { 
+        event.preventDefault(); 
+        wrapper.innerHTML="&#xf00d";
 
-    } else if (regexMailM.test(mailM.value) == false) {
-        event.preventDefault();
-        mod2.innerHTML="&#xf00d";
-
-    } else {
-        event.preventDefault();
-        mod2.innerHTML="&#xf00c";
-    }
-};
-
-
-modal.addEventListener('click', motdepasseModal)
-
-
-function motdepasseModal(event) {
-
-    if (passwordM.validity.valueMissing) {
-        event.preventDefault();
-        mod3.innerHTML="&#xf12a";
-
-    } else if (regexPwdM.test(passwordM.value) == false) {
-        event.preventDefault();
-        mod3.innerHTML="&#xf00d";
-
-    } else {
-        mod3.innerHTML="&#xf00c";
-        event.preventDefault();
-    }
-};
+    } else { 
+        wrapper.innerHTML="&#xf00c";
+    } 
+}
